2bfe6cad775cd3264360eb4cb8c33f27a936f701
fenix <albertfines@gmail.com>
2018-10-20 20:42:01 +0200

    unificar index

--- www/js/index.js
+++ www/js/index.js
@@ -6,25 +6,73 @@ index = (function() {
         document.addEventListener('deviceready', function() {
 
             util.setUUID(function() {
-                var cargarListado = function(){
-
-                    //Una vez tenemos todas las notificaciones, cargamos el listado de notificaciones    
-                    pantallaPrincipal.cargarListado();
-                }
-
                 //una vez activado el usuario, obtenemos todos las notificaciones del servidor
-                pantallaPrincipal.cargaFuegosNotificados_List(function() {
+                cargaFuegosNotificados_List(function() {
                     cargarListado();
+                    cargaMapa();
                 });
             });
-
-
-
         }, false);
     });
 
-    var cargarListado = function() {
+    var cargaFuegosNotificados_List = function (callback){
 
+        var callbackList = function (datosNotificaciones){
+            gestorDatos.setNotificaciones(datosNotificaciones);
+            if(callback){
+                callback();
+            }
+        }
+
+        Services.ServiciosAJAX("getFuegos", "", callbackList);
+    }
+
+    var cargaMapa = function (){
+        var map = L.map('map').setView([41.66, -4.72], 15);
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'OpenStreetMap',
+            maxZoom: 18
+        }).addTo(map);
+
+        L.control.scale().addTo(map);
+
+        var listaCompleta = gestorDatos.getNotificaciones();
+        var limite = (listaCompleta.data.length > 20) ? 20 : listaCompleta.data.length;
+
+        for(var x=0; x < limite; x++) {
+            L.marker([listaCompleta.data[x].lat, listaCompleta.data[x].lon],{}).addTo(map);
+        }
+
+    }
+
+    var cargarListado = function (){
+        var listaCompleta = gestorDatos.getNotificaciones();
+
+        var limite = (listaCompleta.data.length > 20) ? 20 : listaCompleta.data.length;
+
+        var cList = $('#listadoNotificaciones');
+
+        for(var x=0; x < limite; x++){
+
+            var li = $('<li/>')
+                //.addClass('ui-menu-item')
+                //.attr('role', 'menuitem')
+                .appendTo(cList);
+            var aaa = $('<a/>')
+                //.addClass('ui-all')
+                //.text(countries[i])
+                .appendTo(li);
+            var h2 = $('<h2/>')
+                //.addClass('ui-all')
+                .text("LAT: "+listaCompleta.data[x].lat+" - LONG: "+listaCompleta.data[x].lon)
+                .appendTo(aaa);
+            var p = $('<p/>')
+                //.addClass('ui-all')
+                .text(listaCompleta.data[x].comentario)
+                .appendTo(aaa);
+        }
+
+        $('#listadoNotificaciones').listview( "refresh" );
     }
 
     return {
@@ -32,32 +80,3 @@ index = (function() {
     }
 
 })();
-
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-
-        //Cargamos UUID al acceder a la aplicacion
-        util.setUUID();
-
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
-
